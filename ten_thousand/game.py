bfdc5ed89cb8b4b91ab456b1f2cb57b28f849a87
Xin Deng <xind1470@gmail.com>
2024-01-17 18:14:18 -0800

    two test pass

--- ten_thousand/game.py
+++ ten_thousand/game.py
@@ -20,6 +20,51 @@ def play(roller=GameLogic.roll_dice):
                 break  
             round_number += 1
 
+def play_round(roller, total_score, round_number):
+    round_score = 0
+    dice_count = 6
+    quit_game = False  
+
+    while True:
+        dice = roller(dice_count)
+        print(f"Rolling {len(dice)} dice...")
+        print(f"*** {' '.join(map(str, dice))} ***")
+
+        player_choice = banked_dice()
+        if player_choice == "q":
+            print(f"Thanks for playing. You earned {total_score} points")
+            return 0, total_score, True  
+
+        dice_count -= len(player_choice)
+        round_score += GameLogic.calculate_score(player_choice)
+
+        print(f"You have {round_score} unbanked points and {dice_count} dice remaining")
+        choice = players_choice_roll_bank_quit()
+
+        if choice == "b":
+            total_score += round_score
+            print(f"You banked {round_score} points in round {round_number}")
+            print(f"Total score is {total_score} points")
+            return round_score, total_score, False 
+        elif choice == "q":
+            print(f"Thanks for playing. You earned {total_score} points")
+            return 0, total_score, True  
+        elif choice == "r":
+            continue
+
+def banked_dice():
+    print("Enter dice to keep, or (q)uit:")
+    player_input = input("> ")
+    if player_input.lower() == 'q':
+        return player_input
+    try:
+        player_choice = tuple(map(int, player_input))
+        return player_choice
+    except ValueError:
+        print("Invalid input. Please enter a sequence of digits.")
+        return banked_dice()
+
+
 
 if __name__ == "__main__":
     play()
